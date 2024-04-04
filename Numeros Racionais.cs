b811c2dc78ec889e41b32e7b737c8ce6c419b5be
HuanDreesL <163208912+HuanDreesL@users.noreply.github.com>
2024-04-04 19:34:13 -0300

    Numeros Racionais.cs

---
+++ Numeros Racionais.cs
@@ -0,0 +1,12 @@
+int contador = -1;
+int num = 0;
+
+while (num != -1)
+{
+    Console.WriteLine("Digite um Número: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    contador++;
+
+}
+   Console.WriteLine(contador);
+
